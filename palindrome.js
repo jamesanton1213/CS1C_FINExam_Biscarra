6cb9a3d5d2d77a333726799e5d57c630b049fe0a
jamesanton1213 <jamesanthonyragcabiscarra@gmail.com>
2025-05-26 22:08:02 +0800

    palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+let red = prompt("RACECAR");
+let blue = prompt("RECORDER");
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+let reversedRed = reverseString(red);
+let reversedBlue = reverseString(blue);
+
+console.log("Original Word 1:", red);
+console.log("Reversed Word 1:", reversedRed);
+console.log("Original Word 2:", blue);
+console.log("Reversed Word 2:", reversedBlue);
+
+console.log(`Is "${red}" a palindrome?`, red === reversedRed);
+console.log(`Is "${blue}" a palindrome?`, blue === reversedBlue);
